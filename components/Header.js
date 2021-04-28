bd2aa439e814333609e97e224599d0ba7af545de
deddyprianto <haicoders12@gmail.com>
2021-04-28 21:05:26 +0700

    add a new thing

---
+++ components/Header.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+function Header() {
+  return (
+    <header className="h-auto flex flex-col sm:flex-row mt-5 justify-between items-center">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIER" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTION" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <Image
+        className="object-contain"
+        src="/img/hulu.png"
+        width={200}
+        height={100}
+      />
+    </header>
+  );
+}
+
+export default Header;
---
+++ components/Nav.js
@@ -0,0 +1,23 @@
+import request from "../utils/request";
+import { useRouter } from "next/router";
+function Nav() {
+  const router = useRouter();
+  return (
+    <nav className="relative">
+      <div className="flex px-10 sm:px-20 text-1xl whitespace-nowrap space-x-10 sm:space-x-24 overflow-x-scroll scrollbar-hide">
+        {Object.entries(request).map(([key, { title, url }]) => (
+          <h2
+            onClick={() => router.push(`/?genre=${key}`)}
+            key={key}
+            className="cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 last:pr-24"
+          >
+            {title}
+          </h2>
+        ))}
+      </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
+    </nav>
+  );
+}
+
+export default Nav;
