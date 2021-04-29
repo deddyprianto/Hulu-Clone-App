4ca1a85176506540ea3bf07629f1a0e7f5271cde
deddyprianto <haicoders12@gmail.com>
2021-04-29 21:42:54 +0700

    change file result.js in line 5

--- components/Result.js
+++ components/Result.js
@@ -2,7 +2,7 @@ import Thumbnail from "./Thumbnail";
 import FlipMove from "react-flip-move";
 function Result({ result }) {
   return (
-    <div className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 3xl:flex flex-wrap justify-center">
+    <div className="flex items-center justify-center">
       {result.map((data) => (
         <FlipMove key={data.id}>
           <Thumbnail resultThumnail={data} />
